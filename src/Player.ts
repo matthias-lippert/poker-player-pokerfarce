35672c0790d9f0a85349ea1bc5c5a4b0897fd556
ron <ron@ron.de>
2017-02-28 11:05:10 +0100

    minraise instead of 0

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,6 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    betCallback(gameState.minimum_raise);
   }
 
   public showdown(gameState: any): void {
